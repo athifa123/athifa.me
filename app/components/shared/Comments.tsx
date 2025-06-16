16dba4893c455f760cd66decdd2e4fcb6a944093
Athifa Sultana <athifa@Athifas-MacBook-Air.local>
2025-06-17 01:02:23 +0400

    fixed the comments issue

--- app/components/shared/Comments.tsx
+++ app/components/shared/Comments.tsx
@@ -1,35 +1,35 @@
-"use client";
+// "use client";
 
-import { useTheme } from "next-themes";
-import Giscus from "@giscus/react";
-import { giscusRepoId, giscusCategoryId } from "@/lib/env.api";
+// import { useTheme } from "next-themes";
+// import Giscus from "@giscus/react";
+// import { giscusRepoId, giscusCategoryId } from "@/lib/env.api";
 
-export default function Comments() {
-  const { theme } = useTheme();
+// export default function Comments() {
+//   const { theme } = useTheme();
   
-  // Handle undefined theme with fallback
-  const giscusTheme =
-    theme === "light"
-      ? "light"
-      : theme === "dark"
-      ? "transparent_dark"
-      : "dark"; // fallback theme
+//   // Handle undefined theme with fallback
+//   const giscusTheme =
+//     theme === "light"
+//       ? "light"
+//       : theme === "dark"
+//       ? "transparent_dark"
+//       : "dark"; // fallback theme
 
-  return (
-    <Giscus
-      id="comments"
-      repo="athifa123/athifa.me"
-      repoId={giscusRepoId}
-      category="Announcements"
-      categoryId={giscusCategoryId}
-      mapping="title"
-      term="Welcome to @giscus/react component!"
-      reactionsEnabled="1"
-      emitMetadata="0"
-      inputPosition="bottom"
-      theme={giscusTheme}
-      lang="en"
-      loading="lazy"
-    />
-  );
-}
+//   return (
+//     <Giscus
+//       id="comments"
+//       repo="athifa123/athifa.me"
+//       repoId={giscusRepoId}
+//       category="Announcements"
+//       categoryId={giscusCategoryId}
+//       mapping="title"
+//       term="Welcome to @giscus/react component!"
+//       reactionsEnabled="1"
+//       emitMetadata="0"
+//       inputPosition="bottom"
+//       theme={giscusTheme}
+//       lang="en"
+//       loading="lazy"
+//     />
+//   );
+// }
